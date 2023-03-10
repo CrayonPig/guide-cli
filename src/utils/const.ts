c0f89e1fa2b21ba458aef6cebcb4b19b3784f558
CrayonPig <475003925@qq.com>
2023-03-10 18:15:25 +0800

    feat: commit 功能

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -5,11 +5,68 @@ const PKG_NAME = Object.keys(pkg.bin)[0];
 const NPM_PKG_NAME = pkg.name;
 const PKG_VERSION = pkg.version;
 const DEFAULT_CLI_HOME = `.${PKG_NAME}`;
+const GIT_COMMIT_CONFIG: Array<{
+  value: string;
+  name: string;
+  emoji: string;
+}> = [
+  {
+    value: 'feat',
+    name: 'feat:    🚀  新增功能',
+    emoji: '🚀'
+  },
+  {
+    value: 'fix',
+    name: 'fix:      🧩  修复缺陷',
+    emoji: '🧩'
+  },
+  {
+    value: 'docs',
+    name: 'docs:     📚  文档变更',
+    emoji: '📚'
+  },
+  {
+    value: 'style',
+    name: 'style:    🎨  代码格式（不影响功能，例如空格、分号等格式修正）',
+    emoji: '🎨'
+  },
+  {
+    value: 'refactor',
+    name: 'refactor:  ♻️   代码重构（不包括 bug 修复、功能新增）',
+    emoji: '♻️'
+  },
+  {
+    value: 'perf',
+    name: 'perf:     ⚡️  性能优化',
+    emoji: '⚡️'
+  },
+  {
+    value: 'test',
+    name: 'test:     ✅  添加疏漏测试或已有测试改动',
+    emoji: '✅'
+  },
+  {
+    value: 'build',
+    name: 'build:    📦️  构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）',
+    emoji: '📦️'
+  },
+  {
+    value: 'ci',
+    name: 'ci:       🎡  修改 CI 配置、脚本',
+    emoji: '🎡'
+  },
+  {
+    value: 'revert',
+    name: 'revert:   ⏪️  回滚 commit',
+    emoji: '⏪️'
+  }
+];
 
 export {
   LOWEST_NODE_VERSION,
   DEFAULT_CLI_HOME,
   PKG_NAME,
   PKG_VERSION,
-  NPM_PKG_NAME
+  NPM_PKG_NAME,
+  GIT_COMMIT_CONFIG
 };
--- src/utils/git.ts
+++ src/utils/git.ts
@@ -4,14 +4,17 @@ import { simpleGit, SimpleGit } from 'simple-git';
 import ora from 'ora';
 import inquirer from 'inquirer';
 import log from './log';
+import { GIT_COMMIT_CONFIG } from './const';
 
 interface IGitProps {
   dir: string;
 }
 
 class Git {
+  // simpleGit生成的git对象
   git: SimpleGit;
 
+  // 项目路径，默认为当前目录
   dir = '';
 
   constructor(argv?: IGitProps) {
@@ -52,12 +55,13 @@ class Git {
     await this.checkConflicted();
     // 4.检查未提交代码
     await this.checkNotCommitted();
+    // 5.生成Commit
+    await this.createCommitMsg();
   }
 
   async checkStash() {
-    log.info('检查stash记录', '');
+    log.info('开始检查', 'stash记录');
     const stashList = await this.git.stashList();
-
     if (stashList.all.length > 0) {
       const { stashFlag } = (await inquirer.prompt({
         type: 'confirm',
@@ -72,18 +76,20 @@ class Git {
         log.info('忽略stash记录', '');
       }
     }
+    log.success('检查通过', 'stash记录');
   }
 
   async checkConflicted() {
-    log.info('代码冲突检查', '');
+    log.info('开始检查', '代码冲突');
     const status = await this.git.status();
     if (status.conflicted.length > 0) {
       throw new Error('当前代码存在冲突，请手动处理合并后再试！');
     }
-    log.success('代码冲突检查通过');
+    log.success('检查通过', '没有代码冲突');
   }
 
   async checkNotCommitted() {
+    log.info('开始检查', '未提交代码');
     const status = await this.git.status();
     if (status.not_added.length > 0
       || status.created.length > 0
@@ -102,8 +108,49 @@ class Git {
       await this.git.add(status.modified);
       // 重命名的文件， 可通过git mv xx xx出现
       await this.git.add(status.renamed.map((item) => item.to));
+      log.success('已自动将工作区的内容提交的暂存区');
+    }
+    log.success('检查通过', '没有未提交代码');
+  }
 
-      // log.success('本次commit提交成功');
+  async createCommitMsg() {
+    log.info('开始检查', 'commit');
+    // TODO: 后续应与commitLit联动
+    const commitMsg = await this.inputCommitMsg();
+    await this.git.commit(commitMsg);
+    log.success('本次commit提交成功');
+  }
+
+  async inputCommitMsg() {
+    const { commitType, message } = await inquirer.prompt([{
+      type: 'list',
+      name: 'commitType',
+      message: '请选择项目模板',
+      choices: GIT_COMMIT_CONFIG.map((item) => ({
+        value: item.value,
+        name: item.name
+      }))
+    }, {
+      type: 'input',
+      name: 'message',
+      message: '请输入内容:',
+      validate(v: string) {
+        return !!v.trim();
+      }
+    }]);
+    await this.checkCommit(commitType, message);
+    return `${commitType}: ${message}`;
+  }
+
+  async checkCommit(commitType:string, message: string) {
+    const { flag } = await inquirer.prompt({
+      type: 'confirm',
+      name: 'flag',
+      message: `请确认git commit是否为<${commitType}: ${message}>`,
+      default: true
+    });
+    if (!flag) {
+      throw new Error('用户需重新生成git commit');
     }
   }
 }
